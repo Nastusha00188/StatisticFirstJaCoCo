69be54671452d4c30bd252b68ac05e1da6ea6526
Анастасия Киреева <nastusha00188@gmail.com>
2023-01-29 19:48:23 +0300

    Test

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -18,4 +18,15 @@ public class StatisticServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void findSecondMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 15, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 15;
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
